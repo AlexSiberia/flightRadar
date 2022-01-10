25637edd023c505aeebcd68dc24b25dd4d12fc68
AlexKurbatov <AlexKurbatov@icloud.com>
2022-01-10 21:32:26 +0500

    Added first screen and color pallet

--- FlightRadar/ViewController.swift
+++ FlightRadar/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum FirstScreenColor {
+    case backgroundColor
+    case textColor
+}
 
+extension UIColor {
+    static func appColor(_ name: FirstScreenColor) -> UIColor? {
+        switch name {
+        case .backgroundColor:
+            return UIColor(named: "BackgroundColor")
+        case .textColor:
+            return UIColor(named: "TextColor")
+        }
+    }
+}
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let backgroundColor = UIColor.appColor(.backgroundColor)
+        view.backgroundColor = backgroundColor
+        //        view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        
+        let logoImage = UIImage(named: "FirstScreenLogo")
+        let logoImage1 = #imageLiteral(resourceName: "SomeLogo")
+        let logoView = UIImageView(image: logoImage1)
+        logoView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoView)
+        
+        NSLayoutConstraint.activate([
+            logoView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logoView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
     }
-
-
+    
+    
 }
 
